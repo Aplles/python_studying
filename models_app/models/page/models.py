4cb941f960c52bf8d3ce83f9c675c2ebd9b5d2cf
aleksandr razarenov <aleksandr.razarenov@saltpepper.ru>
2023-07-05 23:51:29 +0300

    added models page,user and access

---
+++ models_app/models/page/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import uuid
+
+from django.db import models
+
+
+class Page(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150, verbose_name='Страница')
+    parent_page = models.ForeignKey('self', blank=True, null=True,
+                                    on_delete=models.CASCADE,
+                                    verbose_name='Родительская страница',
+                                    related_name='pages')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'pages'
+        verbose_name = 'Страница'
+        verbose_name_plural = 'Страницы'
