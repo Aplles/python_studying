c34a55471055e964698fd3aab257042f9cda7f4f
SomeBody <maxim.karaichev@saltpepper.ru>
2023-07-06 00:02:26 +0300

    fix init admin

--- conf/settings/django.py
+++ conf/settings/django.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'Moscow/Europe'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
---
+++ models_app/admin/__init__.py
@@ -0,0 +1,6 @@
+from models_app.admin.block.resources import Block
+from models_app.admin.blocks.text_block.resources import TextBlock
+from models_app.admin.blocks.callout_block.resources import CalloutBlock
+from models_app.admin.blocks.image_block.resources import ImageBlock
+from models_app.admin.blocks.link_block.resources import LinkBlock
+from models_app.admin.blocks.divider_block.resources import DividerBlock
