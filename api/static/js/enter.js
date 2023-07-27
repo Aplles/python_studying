668f600e61de7bd998cf65724313ac335fa7046b
SomeBody <maxim.karaichev@saltpepper.ru>
2023-07-27 15:19:18 +0300

    added adaptive

--- api/static/js/enter.js
+++ api/static/js/enter.js
@@ -2,9 +2,11 @@ const wrapper = document.querySelector(".wrapper");
 const mainInfo = document.querySelector(".mainInfo");
 const signInBtn = document.querySelector(".enter");
 const footer = document.querySelector("footer");
+const header = document.querySelector("header");
 
 signInBtn.addEventListener("click", () => {
-  wrapper.classList.add("active");
-  mainInfo.classList.add("noactive");
-  footer.classList.add("noactive");
+    wrapper.classList.add("active");
+    mainInfo.classList.add("noactive");
+    header.classList.add("activet");
+    footer.classList.add("noactive");
 });
